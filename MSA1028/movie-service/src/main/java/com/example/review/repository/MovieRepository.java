75359df34fad1c07c379ca7aa45a28bc41948b1c
sssangho <sangho353@naver.com>
2025-10-28 17:24:28 +0900

    MSA-MOVIE

--- MSA1028/movie-service/src/main/java/com/example/review/repository/MovieRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.review.repository;
-
-import com.example.review.model.Movie;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MovieRepository extends JpaRepository<Movie, Long> {
-}
--- MSA1028/review-service/src/main/java/com/example/review/repository/ReviewRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.review.repository;
-
-import com.example.review.model.Review;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReviewRepository extends JpaRepository<Review, Long> {
-}
